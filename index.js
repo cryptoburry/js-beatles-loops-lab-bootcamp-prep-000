e12757cf76ecc474a468ba25fa167705e584d459
Gardiner Nardini <gardiner.nardini@gmail.com>
2018-04-08 22:41:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 
 function theBeatlesPlay(musicians, instruments){
   var abc = [];
+  var i;
   for(i=0;i<musicians.length;i++){
     abc[i] = `${musicians} plays the ${instruments}`;
   }
   return abc;
 }
 
+function johnLennonFacts(facts){
+  var i = 1
+}
